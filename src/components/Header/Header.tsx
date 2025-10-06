2c0a426b65641b62dd1254f7a61088ee5540445c
LeoGonzaga <sacredlotus05@gmail.com>
2025-10-06 16:54:41 -0300

    feat: atualizando meta tags do projeto

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,11 @@
 import { JSX } from "react";
 import "./styles.scss";
 
-import { useGetBus } from "@src/hooks/useGetBus";
-
 export const Header = (): JSX.Element => {
-  const { updatedAt } = useGetBus();
-
   return (
     <header className="container__header">
       <h1>Horário de ônibus</h1>
-      <span>
-        Atualizado em: <strong>{updatedAt}</strong>
-      </span>
+      <span>Atualizado em: 01/10/2025</span>
     </header>
   );
 };
