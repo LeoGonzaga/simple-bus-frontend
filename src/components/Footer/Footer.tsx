fe6b5b276fe05a347b460e573aba43b59a35869a
LeoGonzaga <sacredlotus05@gmail.com>
2026-05-14 00:11:05 -0300

    fix: adicionando espaço entre os textos do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,14 +5,16 @@ export const Footer = (): JSX.Element => {
   return (
     <footer className="container__footer">
       <p>Notou alguma informação incorreta? </p>
-      <p>Entre em contato comigo!</p>
+      <p>Entre em contato com a gente!</p>
 
       <div className="gap"></div>
 
-      <span>@{new Date().getFullYear()} Desenvolvido por
+      <span>
+        @{new Date().getFullYear()} Desenvolvido por
         <a href="https://www.linkedin.com/in/leogonzaga/" target="_blank">
           Leo Gonzaga
-        </a> e
+        </a>
+        e{" "}
         <a href="https://www.linkedin.com/in/carlosgonzaga11/" target="_blank">
           Carlos Alberto
         </a>
